6b42bd3e9e4a751017f50543f58a2432540e3936
Engine Seunghyun Park <engine@zoyi.co>
2016-10-29 20:14:10 +0900

    Add action 'hamming_calc'

--- src/actions/HammingActionCreator.js
+++ src/actions/HammingActionCreator.js
@@ -2,16 +2,18 @@
  * Created by nariyoon on 2016. 10. 25..
  */
 import Hamming from '../modules/HammingDistCalculator'
-
+import Gimci from '../modules/Gimci'
+import AT from '../constants/ActionTypes'
 
 export function calculate(text1, text2) {
-    return {
-        types: ['HAMMING_REQUEST', 'HAMMING_SUCCESS', 'HAMMING_FAILURE'],
-        promise: () => {
+  return {
+    types: [AT.HAMMING_CALC_REQUEST, AT.HAMMING_CALC_SUCCESS, AT.HAMMING_CALC_FAILURE],
+    promise: () => {
 
-            return new Promise((resolve, reject) => {
-                resolve(Hamming(text1, text2))
-            })
-        }
+      return new Promise((resolve, reject) => {
+
+        resolve(Hamming(text1, text2))
+      })
     }
+  }
 }
--- src/reducers/HammingReducer.js
+++ src/reducers/HammingReducer.js
@@ -1,21 +1,20 @@
-/**
- * Created by nariyoon on 2016. 10. 25..
- */
+import AT from '../constants/ActionTypes'
+
 export default function _hamming(state = {}, action) {
     console.log('_hamming', state, action);
 
     switch (action.type) {
-        case 'HAMMING_REQUEST':
+        case AT.HAMMING_CALC_REQUEST:
             return {
                 ...state
             }
-        case 'HAMMING_SUCCESS':
+        case AT.HAMMING_CALC_SUCCESS:
             console.log(2, action)
             return {
                 ...state,
                 converted: action.result
             }
-        case 'HAMMING_FAILURE':
+        case AT.HAMMING_CALC_FAILURE:
             return {
                 ...state,
             }
