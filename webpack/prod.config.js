8a12f092da87ac61f915cebc72967e545455020f
Engine Seunghyun Park <engine@zoyi.co>
2016-11-19 03:00:53 +0900

    Modify webpack configurations

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,34 +1,33 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require('webpack')
+var autoprefixer = require('autoprefixer')
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-var BUILD_DIR = path.resolve(__dirname, 'build');
-var APP_DIR = path.resolve(__dirname, 'src');
-
-var config = {
-  devtool: "source-map",
-  entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    APP_DIR + '/main.js',
-    
-  ],
+module.exports = {
+  entry: {
+    app: './src/main.js'
+  },
   output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
+    path: './build',
+    filename: "bundle.js"
   },
   plugins: [
-    new ExtractTextPlugin('style.css', { allChunks: true }),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.ProvidePlugin({
-      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+    new ExtractTextPlugin('style.css', {allChunks: true}),
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({minimize: true}),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
     })
   ],
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    modulesDirectories: ['node_modules']
   },
-  module : {
-    loaders : [
+  node: {
+    net: 'empty',
+    dns: 'empty',
+    fs: 'empty'
+  },
+  module: {
+    loaders: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
@@ -37,6 +36,7 @@ var config = {
           presets:[ 'es2015', 'react', 'stage-2' ]
         }
       },
+
       {
         test: /\.js?$/,
         exclude: /node_modules/,
@@ -45,19 +45,41 @@ var config = {
           presets:[ 'es2015', 'react', 'stage-2' ]
         }
       },
+
       {
         test: /\.scss$/,
         loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass!postcss-loader')
       },
-      { test: /\.css$/, 
-        loader: ExtractTextPlugin.extract("style-loader", "css-loader") 
+
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      },
+
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader?limit=8192'
+      },
+
+      {
+        test: /\.html$/,
+        loader: "file?name=[path][name].[ext]&context=./src"
+      },
+
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+
+      { test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
       }
     ]
   },
-  devServer: {
-    historyApiFallback: true,
-    headers: { "Access-Control-Allow-Origin": "*" }
-  }
-};
-
-module.exports = config;
+  postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ]
+}
